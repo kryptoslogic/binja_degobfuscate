8bfd86414c65d81022035f527292e4a9aed59b83
Jordan Wiens <jordan@psifertex.com>
2020-11-29 15:42:06 -0500

    initial conversion to a bn plugin

---
+++ README.md
@@ -0,0 +1,44 @@
+# Degobfuscate (v1.0)
+Author: **jamie-34254**
+
+_De-obfuscates strings inside of obfuscated GO binaries_
+
+## Description:
+
+This plugin implements a simple LLIL emulator to statically de-obfuscate simple string obfuscation such as the obfuscations done by [gobfuscate](https://github.com/unixpickle/gobfuscate).
+
+
+## Installation Instructions
+
+### Darwin
+
+no special instructions, package manager is recommended
+
+### Linux
+
+no special instructions, package manager is recommended
+
+### Windows
+
+no special instructions, package manager is recommended
+
+## Minimum Version
+
+This plugin requires the following minimum version of Binary Ninja:
+
+* 1528
+
+
+
+## Required Dependencies
+
+The following dependencies are required for this plugin:
+
+
+
+## License
+
+This plugin is released under a MIT license.
+## Metadata Version
+
+2
--- __init__.py
+++ __init__.py
@@ -1,81 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[57]:
-
-
 import os.path
-import logging
 import struct
-from termcolor import colored
-from io import BytesIO
-
 from binaryninja import * 
-log_to_stderr(LogLevel.ErrorLog)
-print(core_version())
 
-
-# In[2]:
-
-
-# Create DB from scratch
-h = "5c9b30d502e2f103f089607ce699520f88154e3d7988a9db801f2a2a4378bf41"
-
-if os.path.exists(f"./testcases/{h}.bndb"):
-    print("Loading existing DB")
-    fm = binaryninja.FileMetadata()
-    db = fm.open_existing_database(f"./testcases/{h}.bndb")
-
-    if db is None:
-        raise Exception("Database doesn't exist")
-
-    bv = db.get_view_of_type('PE') or db.get_view_of_type('ELF')
-elif os.path.exists(f"./testcases/{h}"):
-    print("Creating DB")
-    bv = binaryninja.BinaryViewType.get_view_of_file_with_options(f"./testcases/{h}")
-else:
-    raise Exception("File doesn't exist...")
-
-
-# In[3]:
-
-
-morestack_noctxt_sym = bv.get_symbols_by_name("runtime.morestack_noctxt") or bv.get_symbols_by_name("_runtime.morestack_noctxt") or bv.get_symbols_by_name("runtime_morestack_noctxt") or bv.get_symbols_by_name("_runtime_morestack_noctxt")
-slicebytetostring_sym = bv.get_symbols_by_name("runtime.slicebytetostring") or bv.get_symbols_by_name("_runtime.slicebytetostring") or bv.get_symbols_by_name("runtime_slicebytetostring") or bv.get_symbols_by_name("_runtime_slicebytetostring")
-
-morestack_noctxt = bv.get_function_at(morestack_noctxt_sym[0].address)
-slicebytetostring = bv.get_function_at(slicebytetostring_sym[0].address)
-
-deobfuscate_candidates = set()
-
-print("{} functions".format(len(bv.functions)))
-
-for func in bv.functions:
-    func_callees = func.callees
-    if (len(func_callees)) == 2:
-        if morestack_noctxt in func_callees and slicebytetostring in func_callees:
-            print(func.name)
-            for ins in func.instructions:
-                ins_type = ins[0][0]
-                if ins_type.text != "xor":
-                    continue
-                src = ins[0][2]
-                dst  = ins[0][4]
-                if src == "eax" and dst == "eax":
-                    continue
-                
-                deobfuscate_candidates.add(func)
-                break
-            
-           
-
-
-# In[91]:
-
-
-logger = logging.getLogger("parser")
-logger.setLevel(logging.DEBUG)
-#logging.getLogger().setLevel(logging.INFO)
+morestack_noctxt_sym = None
+slicebytetostring_sym = None
+morestack_noctxt = None
+slicebytetostring = None
 
 def handle_deref(bv, deref_size, deref_addr):
     br = binaryninja.BinaryReader(bv)
@@ -91,6 +24,7 @@ class EmuMagic(object):
     # What endianness does our arch use
     endianness = None
 
+    #TODO: Remove arch specific code
     registers = {
         "fsbase": 100,
         "rcx": 0,
@@ -126,7 +60,7 @@ class EmuMagic(object):
     def handle_LLIL_XOR(self, inst):
         left = self.handle(inst.left)
         right = self.handle(inst.right)
-        print(chr(left ^ right))
+        log_info("Degobfuscate: XOR " + chr(left ^ right))
         return left ^ right
     
     def handle_LLIL_ZX(self, expr):
@@ -167,7 +101,7 @@ class EmuMagic(object):
         src = self.handle(inst.src)
         reg_name = str(inst.dest)
         self.registers[reg_name] = src
-        #print("{}={}".format(reg_name, hex(src)))
+        log_debug(f"Degobfuscate: {reg_name}={hex(src)}")
         return True
     
     def handle_LLIL_CONST(self, inst):
@@ -184,12 +118,12 @@ class EmuMagic(object):
         return self.handle(inst.left) + self.handle(inst.right)
 
     def handle(self, inst):
-        #print("Executing: {}".format(str(inst.operation)))
+        log_debug(f"Degobfuscate executing: {inst.operation.name}")
         for field in LowLevelILInstruction.ILOperations[inst.operation]:
-            handler = "handle_{}".format(inst.operation.name)
+            handler = f"handle_{inst.operation.name}"
             has_handler = hasattr(self, handler)
             if has_handler is False:
-                print("Implement: {}".format(str(inst.operation)))
+                log_info(f"Degobfuscate implement: {inst.operation.name}")
                 return None
             else:
                 res = getattr(self, handler)(inst)
@@ -198,7 +132,7 @@ class EmuMagic(object):
     def execute(self):
         if self.ip >= len(self.instructions):
             return False
-        #print(self.ip)
+        log_debug(f"Degobfuscate IP: {hex(self.ip)}")
         instr = self.instructions[self.ip]
         self.ip += 1
         self.handle(instr)
@@ -206,25 +140,57 @@ class EmuMagic(object):
     
     def run(self):
         while True:
-            if not emu.execute():
+            if not self.execute():
                 break
-        print("We're done!")
     
     def __init__(self, candidate):
         self.endianness = candidate.arch.endianness
         self.instructions = candidate.llil
 
-for candidate in deobfuscate_candidates:
-    emu = EmuMagic(candidate)
-    # candidate.name != "fpkkfenfnamejfgjlfoj_accdmbdpffpomfofhdmd_eamfeondkolifgkfpmjm_glob_func7" and
-    if candidate.name != "fpkkfenfnamejfgjlfoj_accdmbdpffpomfofhdmd_nebiohfnhlhlklappnlj_Ddfcadiihliaahccbolg_func5":
-        continue
-    print(candidate.name)
+def findslice(bv):
+    global morestack_noctxt_sym
+    global slicebytetostring_sym
+    global morestack_noctxt
+    global slicebytetostring
+    morestack_noctxt_sym = bv.get_symbols_by_name("runtime.morestack_noctxt") or bv.get_symbols_by_name("_runtime.morestack_noctxt") or bv.get_symbols_by_name("runtime_morestack_noctxt") or bv.get_symbols_by_name("_runtime_morestack_noctxt")
+    slicebytetostring_sym = bv.get_symbols_by_name("runtime.slicebytetostring") or bv.get_symbols_by_name("_runtime.slicebytetostring") or bv.get_symbols_by_name("runtime_slicebytetostring") or bv.get_symbols_by_name("_runtime_slicebytetostring")
+    morestack_noctxt = bv.get_function_at(morestack_noctxt_sym[0].address)
+    slicebytetostring = bv.get_function_at(slicebytetostring_sym[0].address)
+
+
+def deobfunc(bv, func):
+    if not slicebytetostring:
+        findslice(bv)
+    emu = EmuMagic(func)
+    log_info(f"Degobfuscate analyzing {func.name}")
     emu.run()
 
 
-# In[ ]:
+def deob(bv):
+    findslice(bv)
+    deobfuscate_candidates = set()
 
+    log_info(f"Degobfuscate {len(bv.functions)} total functions")
 
+    for func in bv.functions:
+        func_callees = func.callees
+        if (len(func_callees)) == 2:
+            if morestack_noctxt in func_callees and slicebytetostring in func_callees:
+                log_info(func.name)
+                for ins in func.instructions:
+                    ins_type = ins[0][0]
+                    if ins_type.text != "xor": #TODO: Remove arch specific code
+                        continue
+                    src = ins[0][2]
+                    dst  = ins[0][4]
+                    if src == "eax" and dst == "eax": #TODO: Remove arch specific code
+                        continue
+                    
+                    deobfuscate_candidates.add(func)
+                    break
 
+    for candidate in deobfuscate_candidates:
+        deobfunc(bv, f)
 
+PluginCommand.register_for_function("Degobfuscate function strings", "Searches Symgrate2 db for the current function.", deobfunc)
+PluginCommand.register("Degobfuscate all strings", "Searches all functions for obfuscated xor strings and attempts light IL emulation to recover them.", deob)
